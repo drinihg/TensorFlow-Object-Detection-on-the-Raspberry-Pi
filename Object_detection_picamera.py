5815a4fd02ff25616ef13ae330f4f624f409eacf
drinihg <75312464+drinihg@users.noreply.github.com>
2020-12-01 14:04:59 +0100

    Update Object_detection_picamera.py

--- Object_detection_picamera.py
+++ Object_detection_picamera.py
@@ -27,6 +27,7 @@ from picamera import PiCamera
 import tensorflow as tf
 import argparse
 import sys
+import tensorflow.compat.v1 as tf
 
 # Set up camera constants
 IM_WIDTH = 1280
@@ -79,8 +80,8 @@ category_index = label_map_util.create_category_index(categories)
 # Load the Tensorflow model into memory.
 detection_graph = tf.Graph()
 with detection_graph.as_default():
-    od_graph_def = tf.GraphDef()
-    with tf.gfile.GFile(PATH_TO_CKPT, 'rb') as fid:
+    od_graph_def = tf.compat.v1.GraphDef()
+    with tf.io.gfile.GFile(PATH_TO_CKPT, 'r') as fid:
         serialized_graph = fid.read()
         od_graph_def.ParseFromString(serialized_graph)
         tf.import_graph_def(od_graph_def, name='')
